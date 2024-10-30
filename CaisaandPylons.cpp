5aaad1a8b80e48d0799b20ab7c155aff767fbf2a
viveak910 <1602-23-737-191@vce.ac.in>
2024-10-30 22:55:58 +0530

    cat: math caisaandpylons

--- CaisaandPylons.cpp
+++ CaisaandPylons.cpp
@@ -38,7 +38,19 @@ int main() {
     int t = 1;
     // cin >> t; // Uncomment if multiple test cases
     while (t--) {
-        // Solution code here
+    int  x = 0, y, n, sol = 0,energy = 0;
+    cin >> n;
+    for(int i = 1;i <= n; ++i)
+    {
+        cin >> y;
+        energy += x-y;
+        if(energy < 0){
+            sol += -energy;
+            energy = 0;
+        }
+        x = y;
+    }
+    cout<<sol<<"\n";
     }
 
     return 0;
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # competitive-programming
 I will be uploading the source code (solutions to the questions from the Codeforces Striver CP Sheet).
+
